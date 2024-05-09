cdff2cde86c86b6d7a3f85005f8521e41fee6cb1
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-05-09 16:06:02 +0530

    important change in setup.py
    Added custom install command function which mainly installs everything
    for us. Changes in setup() mainly version and entry point.

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 import os
-import shutil
-from setuptools import setup
+import shutil, subprocess
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+class CustomInstallCommand(install):
+    def run(self):
+        install.run(self)  # Run standard install logic
+        post_install()
 
 def install_man_page():
     source_path = os.path.join('docs', 'man', 'cheatshh.1')
-    dest_path = os.path.join('share', 'man', 'man1', 'cheatshh.1')
-    shutil.copy(source_path, dest_path)
+    dest_path = os.path.join('/usr/local/','share', 'man', 'man1', 'cheatshh.1')
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    try:
+        shutil.copy(source_path, dest_path)
+    except PermissionError:
+        print("Permission denied. Please run this script as root for man pages.")
 
-def install_tldr_page():
-    source_path = os.path.join('docs', 'tldr', 'cheatshh.md')
-    dest_path = os.path.join('share', 'tldr', 'pages', 'cheatshh.md')
-    shutil.copy(source_path, dest_path)
 
 def post_install():
     install_man_page()
-    install_tldr_page()
     # Define the path to ~/.config/cheatshh
     config_dir = os.path.expanduser("~/.config/cheatshh")
 
@@ -22,21 +27,18 @@ def post_install():
     os.makedirs(config_dir, exist_ok=True)
 
     # Copy files to ~/.config/cheatshh
-    files_to_copy = ["cheats.sh", "commands.json", "groups.json"]
+    files_to_copy = ["cheats.sh", "commands.json", "groups.json", "README.md", "requirements.txt"]
     for file_name in files_to_copy:
         with open(file_name, "rb") as src:
             with open(os.path.join(config_dir, file_name), "wb") as dest:
                 dest.write(src.read())
 
-    # Create a symbolic link to cheats.sh in ~/.local/bin
-    os.symlink(
-        os.path.join(config_dir, "cheats.sh"),
-        os.path.expanduser("~/.local/bin/cheatshh"),
-    )
+def run_cheatshh():
+    subprocess.run(['bash', '~/.config/cheatshh/cheats.sh'])
 
 
-setup(name="cheatshh", version="1.0.0", cmdclass={"install": post_install}, 
-      long_description="""
+setup(name="cheatshh", version="1.0.2", cmdclass={"install": CustomInstallCommand}, 
+    long_description="""
 # cheatshh
 
 Cheatshh is an interactive CLI meant for managing command line cheatshheets. Now you don't have to remember CLI commands and just refer your cheatshhet. You can group commands and view their TLDR and MAN pages along with a custom description for the command.
@@ -53,17 +55,23 @@ Cheatshh is an interactive CLI meant for managing command line cheatshheets. Now
 Visit the Github Repository for more details: https://github.com/AnirudhG07/cheatshh
 
 # Version
-1.0.0
+1.0.2
 
 ## Note:
 - This package is best used in Unix based systems, like linux and MacOS. For Windows, see the github repository for more details.
-- The package is installed in ~/.local/bin/cheatshh, so make sure to add this to your PATH variable.
+- The package is installed in ~/.config/cheatshh directory.
 
 
 """,
-      long_description_content_type="text/markdown",
-      keywords=["cheatsheet, cheat, command-line, cli"],
-      install_requires=["fuzzyfinder", "whiptail"],
-      author="Anirudh Gupta"
+    long_description_content_type="text/markdown",
+    keywords=["cheatsheet, cheat, command-line, cli"],
+    install_requires=["fuzzyfinder", "whiptail"],
+    author="Anirudh Gupta",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'cheatshh=src.run_cheatshh:main',
+        ],
+    },
 )
 
