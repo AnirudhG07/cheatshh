4f8a7556e9d27c3bd0923495bebd6fc18b16e9ed
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-06-27 15:57:45 +0530

    main.py configured for HOME instead of ~

--- cheatshh/main.py
+++ cheatshh/main.py
@@ -6,29 +6,33 @@ import sys
 import argparse
 
 def post_install():
+    config_path = os.path.join(os.path.expanduser('~'), '.config', 'cheatshh')
     try:
         # Check if the directory exists
-        if not os.path.exists(os.path.expanduser('~/.config/cheatshh')):
+        if not os.path.exists(config_path):
             # If not, create it
-            os.makedirs(os.path.expanduser('~/.config/cheatshh'))
-            # Copy the files from the package to ~/.config/cheatshh
+            os.makedirs(config_path)
+            # Copy the files from the package to the new config path
             for file_name in ['cheats.sh', 'groups.json', 'commands.json', 'cheatshh.toml']:
                 file_path = pkg_resources.resource_filename(__name__, file_name)
-                shutil.copy2(file_path, os.path.expanduser('~/.config/cheatshh/' + file_name))
+                shutil.copy2(file_path, os.path.join(config_path, file_name))
             # Make cheats.sh executable
-            subprocess.call(['chmod', '+x', os.path.expanduser('~/.config/cheatshh/cheats.sh')])
+            subprocess.call(['chmod', '+x', os.path.join(config_path, 'cheats.sh')])
     except:
-        print("Error: Could not create ~/.config/cheatshh directory. Please retry or report issue to official Github page.")
+        print("Error: Could not create {} directory. Please retry or report issue to official Github page.".format(config_path))
     return
 
 def cheatshh(args):
-    subprocess.call([os.path.expanduser('~/.config/cheatshh/cheats.sh')]+args)
+    try:
+        subprocess.call([os.path.join(os.path.expanduser('~'), '.config', 'cheatshh', 'cheats.sh')] + args)
+    except Exception as e:
+        print(f"Error: {e}.\n Could not run cheatshh.")
 
 def main():
     """
     Cheatshh main function simply calls cheats.sh function which is the main function of cheats.sh.
     It is a simple wrapper around cheats.sh using python.
-    This function will also create ~/.config/cheatshh directory if it does not exist.
+    This function will also create $HOME/.config/cheatshh directory if it does not exist.
     """
     post_install()
     cheatshh(sys.argv[1:])
